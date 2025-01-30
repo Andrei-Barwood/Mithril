dc8aa185f5487350cc5a28117e7b396d9071b9bb
System Administrator <root@MacBook-Air-de-Andres.local>
2025-01-30 16:00:07 -0300

    (Operaciones Lógicas) Mithril 10.6-1 - (programa) vector de bits para operador OR. Entrada: a_l y b_l son los argumentos sobre los cuales se opera. Salida: c_l es el valor de la operación OR

---
+++ 10.0/10 - bitwise OR.c
@@ -0,0 +1,31 @@
+#include "flint.h"
+void or_l (CLINT a_l, CLINT b_l, CLINT c_l) {
+	CLINT d_l;
+	clint *r_l, *s_l, *t_l;
+	clint *msdptrr_l;
+	clint *msdptrs_l;
+
+	if (DIGITS_L (a_l) < DIGITS_L (b_l)) {
+		r_l = LSDPTR_L (b_l);
+		s_l = LSDPTR_L (a_l);
+		msdptrr_l = MSDPTR_L (b_l);
+		msdptrs_l = MSDPTR_L (a_l);
+	}
+	else {
+		r_l = LSDPTR_L (a_l);
+		s_l = LSDPTR_L (b_l);
+		msdptrr_l = MSDPTR_L (a_l);
+		msdptrr_l = MSDPTR_L (b_l);
+	}
+
+	t_l = LSDPTR_L (d_l);
+	SETDIGITS_L (d_l, DIGITS_L (r_l - 1));
+
+	while (s_l <= msdptrs_l) {
+		*t_l++ = *r_l++ | *s_l++;
+	}
+	while (r_l <= msdptrr_l) {
+		*t_l++ = *r_l++;
+	}
+	cpy_l (c_l, d_l);
+}
