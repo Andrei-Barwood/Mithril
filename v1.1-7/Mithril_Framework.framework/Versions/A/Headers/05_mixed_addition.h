697d2b4e211b8278057f7f62ff4c8347dd6abcb7
Andres Barbudo Rodriguez <andresbarbudo@icloud.com>
2025-09-03 07:33:48 -0400

    v1.1-7 macOS framework

---
+++ v1.1-7/Mithril_Framework.framework/Versions/A/Headers/05_mixed_addition.h
@@ -0,0 +1,25 @@
+//
+//  05_mixed_addition.h
+//  Mithril_Criptography_API
+//
+//  Created by Andres Barbudo on 21-08-25.
+//
+
+#ifndef MODERN_MIXED_ADDITION_H
+#define MODERN_MIXED_ADDITION_H
+
+#include "flint.h"
+#include "fmpz.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Just use FLINT's built-in function directly
+// No need for custom wrapper functions
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
